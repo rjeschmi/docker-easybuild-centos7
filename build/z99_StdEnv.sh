06c620cb51f0c9925e7f1b0ba63525fca32f6a5e
Robert Schmidt <rjeschmi@gmail.com>
2015-01-06 09:07:31 -0500

    adding export modulepath

--- build/z99_StdEnv.sh
+++ build/z99_StdEnv.sh
@@ -1,6 +1,7 @@
 
 export MODULEPATH=$(/software/Lmod/lmod/lmod/libexec/addto --append MODULEPATH /software/easybuild-develop/modulefiles)
 export MODULEPATH=$(/software/Lmod/lmod/lmod/libexec/addto --append MODULEPATH /software/easybuild/modules/all)
+export MODULEPATH=$(/software/Lmod/lmod/lmod/libexec/addto --append MODULEPATH /export/easybuild/modules/all)
 export EASYBUILD_CONFIGFILES=/software/config/config.cfg
 
 module load EasyBuild
