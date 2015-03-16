6a6fa32e7d8ac975885e0dc5ce70ba718335d016
Robert Schmidt <rjeschmi@gmail.com>
2015-03-15 21:10:00 -0400

    use 2.0 easybuild docker script

--- easybuild-docker.sh
+++ easybuild-docker.sh
@@ -6,6 +6,7 @@ source /etc/profile.d/z99_StdEnv.sh
 GITHUB_USER=${GITHUB_USER:-}
 GITHUB_TOKEN=${GITHUB_TOKEN:-}
 GITHUB_OPT=${GITHUB_USER:+"--upload-test-report --github-user=${GITHUB_USER} --test-report-env-filter='^SSH|USER|HOSTNAME|UID|.*COOKIE.*'|GITHUB_TOKEN"}
+EB_FILES=${EB_FILES:-}
 
 FROM_PR=${FROM_PR:-}
 FROM_PR_OPT=${FROM_PR:+"--from-pr=${FROM_PR}"}
@@ -13,5 +14,5 @@ FROM_PR_OPT=${FROM_PR:+"--from-pr=${FROM_PR}"}
 
 python -c "import getpass, keyring; keyring.set_password('github_token', '${GITHUB_USER}', '${GITHUB_TOKEN}')"
 
-eb ${FROM_PR_OPT} ${GITHUB_OPT} -dfr
+eb ${EB_FILES} ${FROM_PR_OPT} ${GITHUB_OPT} -dfr
 
