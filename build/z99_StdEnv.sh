9427d851d1214e95e2e6687e26c6c9e1633c236e
Robert Schmidt <rjeschmi@gmail.com>
2017-02-08 10:52:19 +0000

    fixing some bugs

--- build/z99_StdEnv.sh
+++ build/z99_StdEnv.sh
@@ -1,9 +1,10 @@
 
 export MODULEPATH=$(/easybuild/deps/lmod/lmod/libexec/addto --append MODULEPATH /easybuild/easybuild-develop/modulefiles)
 export MODULEPATH=$(/easybuild/deps/lmod/lmod/libexec/addto --append MODULEPATH /easybuild/modules/all/Core)
+export MODULEPATH=$(/easybuild/deps/lmod/lmod/libexec/addto --append MODULEPATH /easybuild/modules/all)
 
 if [ -z "$__Init_Default_Modules" -o -z "$LD_LIBRARY_PATH" ]; then
   __Init_Default_Modules=1; export __Init_Default_Modules;
-    module getdefault default || module load StdEnv
+    module load StdEnv
 fi
 
